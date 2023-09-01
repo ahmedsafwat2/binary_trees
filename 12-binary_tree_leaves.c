d44040ff7cb661069d9a9305466ec819a957a032
ahmed safwat <asosnses@gmail.com>
2023-09-01 05:08:50 +0300

    task12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaf = 0, l = 0, r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+    if (tree->left == NULL && tree->left == NULL)
+    {
+        return (1);
+    }
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
